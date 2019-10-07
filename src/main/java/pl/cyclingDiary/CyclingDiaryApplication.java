cf0682b8420b276ad84800659d4c570befc69681
arek111126 <arek111126@gmail.com>
2019-10-08 01:35:58 +0200

    initial commit

---
+++ src/main/java/pl/cyclingDiary/CyclingDiaryApplication.java
@@ -0,0 +1,15 @@
+package pl.cyclingDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CyclingDiaryApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyclingDiaryApplication.class, args);
+    }
+
+}
